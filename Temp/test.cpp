7680bdc99b6e1ae4fc257021d39f496dc912b135
ayush-rusau <ayush.rusau@gmail.com>
2019-03-04 11:11:22 +0530

    .

--- Temp/test.cpp
+++ Temp/test.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define rep(i, n) for(int i = 0;i<n;i++)
+#define pi pair(ll, ll)
+#define pb push_back
+#define mod 1000000007
+#define max 1000000000
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define mk make_pair
+#define size1 100000001
+// vector<bool> sieve(size1, true);
 
 int main() {
-    cout << "hy Bro" ;
-    return 0;
-}
+    
+    
+} 
